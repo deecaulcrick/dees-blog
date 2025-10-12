92e372950fb3a1b1143704a8bbbe1e7d358ed0b8
Dee Caulcrick <deecaulcrick@gmail.com>
2025-10-12 08:20:04 +0100

    Edited Nav Menu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -15,37 +15,6 @@ const NavMenu = () => {
     return (
         <NavigationMenu viewport={false}>
             <NavigationMenuList>
-                {/* <NavigationMenuItem>
-                    <NavigationMenuTrigger>Goodies</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <Link
-                                        className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
-                                        href="/"
-                                    >
-                                        <div className="mt-4 mb-2 text-lg font-medium">
-                                            Notion templates
-                                        </div>
-                                        <p className="text-muted-foreground text-sm leading-tight">
-                                            Beautifully designed templates built with Notion.
-                                        </p>
-                                    </Link>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/docs" title="Introduction">
-                                Re-usable components built using Radix UI and Tailwind CSS.
-                            </ListItem>
-                            <ListItem href="/docs/installation" title="Installation">
-                                How to install dependencies and structure your app.
-                            </ListItem>
-                            <ListItem href="/docs/primitives/typography" title="Typography">
-                                Styles for headings, paragraphs, lists...etc
-                            </ListItem>
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem> */}
 
                 <NavigationMenuItem>
                     <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
@@ -68,9 +37,11 @@ const NavMenu = () => {
                         <Link href="/about">About </Link>
                     </NavigationMenuLink>
                 </NavigationMenuItem>
-
-
-
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <Link href="/about">About </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
     )
